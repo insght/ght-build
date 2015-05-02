4f39f6ad569a51a516f70b63287d8480ab6e0817
Den Is <umozzie@gmail.com>
2015-05-02 13:15:47 +0300

    v0.1.5
    v0.1.5

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 /** ght-build alpha */
 var fs		= require('fs');
+var path	= require('path');
 var mkdirp	= require('mkdirp');
 var exec	= require('child_process').exec;
-var settings =
-	JSON.parse(
-		require('fs').readFileSync(
-			require('path').resolve(
-				__dirname,
-				'ght-project.json'),
-			'utf8'));
+var settings = JSON.parse(
+	fs.readFileSync(
+		path.resolve(
+			__dirname,
+			'ght-project.json'
+		),
+		'utf8'
+	)
+);
 
 module.exports = function() {
+	var themeName = settings.name;
+	settings.fileAndDirs.forEach(function(data){
+		console.log(data);
+	});
+	mkdirp('skin/frontend/' + themeName, function(err) {
+		mkdirp('skin/frontend/' + themeName + '/default', function(err) {
 
-
-		var dataJSON  = settings;
-		var themeName = dataJSON.name;
-
-		mkdirp('skin/frontend/' + themeName, function(err) {
-			mkdirp('skin/frontend/' + themeName + '/default', function(err) {
-			});
 		});
-
-}
+	});
+};
