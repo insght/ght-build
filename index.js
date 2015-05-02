049cfc702f384ff57786c3056313b1f25b8da4f2
Den Is <umozzie@gmail.com>
2015-05-02 12:44:08 +0300

    Make skin path
    Make skin path

--- index.js
+++ index.js
@@ -2,10 +2,30 @@
 var fs = require('fs');
 
 module.exports = function() {
+	var exec = require('child_process').exec;
+
 	fs.readFile('ght-project.json', 'utf8', function (err,data) {
 		if (err) {
 			return console.log(err);
 		}
-		console.log(data);
+
+		var dataJSON  = JSON.parse(data);
+		var themeName = dataJSON.name;
+
+		exec('mkdir skin/frontend/' + themeName, function (err, stdout, stderr) {
+			if (err) {
+				return console.log(err);
+			}
+
+			exec('mkdir skin/frontend/' + themeName + '/default', function (err, stdout, stderr) {
+				if (err) {
+					return console.log(err);
+				}
+
+				console.log(stdout);
+			});
+
+			console.log(stdout);
+		});
 	});
 }
